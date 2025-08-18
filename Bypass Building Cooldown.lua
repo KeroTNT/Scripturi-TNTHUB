8c8ca2f1fe5bb5345a5dcd6312dc83fe00021bbe
Kero.exe <100468878+KeroTNT@users.noreply.github.com>
2025-08-18 14:30:58 +0200

    Create Bypass Building Cooldown.lua

---
+++ Bypass Building Cooldown.lua
@@ -0,0 +1,1 @@
+game:GetService('ReplicatedStorage')['ACS_Engine'].Eventos.Recarregar:FireServer(9999999,{ACS_Modulo={Variaveis={StoredAmmo = game.Players.LocalPlayer.Character.ACS_Client.Kit.Fortifications}}})
