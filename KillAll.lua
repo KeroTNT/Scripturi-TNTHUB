3a71576393b95d19027bd271874f5a29b48d903f
Kero.exe <100468878+Kerutzuu@users.noreply.github.com>
2025-08-27 20:01:12 +0200

    Update KillAll.lua

--- KillAll.lua
+++ KillAll.lua
@@ -1,16 +1,1 @@
-local plrs = game:GetService("Players")
-
-local function killall()
-	local pname = game.Players.LocalPlayer.Name
-	for _, player in pairs(plrs:GetPlayers()) do
-		if player.Character and player.Character:FindFirstChild("Humanoid") then
-			local tbl_main = {
-				player.Character.Humanoid,
-				math.huge,
-				0,
-				0
-			}
-			game:GetService("ReplicatedStorage").ACS_Engine.Eventos.Damage:FireServer(unpack(tbl_main))
-		end
-	end
-end
+game:GetService('ReplicatedStorage')['ACS_Engine'].Eventos.Damage:FireServer(game.Players.LocalPlayer.Character.Humanoid, math.huge, 0, 0)
